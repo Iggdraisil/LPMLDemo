7f684100a7bac0f2577af0886aec7c890723fccd
Oleh Paduchak <padychak.o@gmail.com>
2020-04-24 22:39:34 +0300

    fixed proble with dialog + added comments for better understandin

--- app/src/main/java/com/olezhko/lpmldemo/LoginActivity.java
+++ app/src/main/java/com/olezhko/lpmldemo/LoginActivity.java
@@ -19,24 +19,32 @@ public class LoginActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        //initializing UI
         setContentView(R.layout.activity_login);
         signInButton = findViewById(R.id.sign_in);
         emailEditText = findViewById(R.id.email);
         passwordEditText = findViewById(R.id.password);
         checkBox = findViewById(R.id.slave);
+        //setting onClickListener to execute following code as user clicks button
         signInButton.setOnClickListener(view -> {
+            //getting data from editTexts and checkbox
             String email = emailEditText.getText().toString();
             String password = passwordEditText.getText().toString();
             boolean slave = checkBox.isChecked();
+            //checking if password and email are proper enough
             if (password.length() < 9 && email.length() < 10) {
                 Toast.makeText(getApplicationContext(),"Довше строчи", Toast.LENGTH_LONG).show();
             } else {
+                //checking checkbox value
                 if (!slave) {
                     Toast.makeText(getApplicationContext(),"стань рабом, тоді впустим", Toast.LENGTH_LONG).show();
                     return;
                 }
+                //creating intent
                 Intent signInIntent = new Intent(LoginActivity.this, MainActivity.class);
+                //adding our emil and password into intent to receive it in MainActivity
                 signInIntent.putExtra("userData", new String[]{email, password});
+                //starting MainActivity with this intent
                 startActivity(signInIntent);
             }
         });
--- app/src/main/java/com/olezhko/lpmldemo/MainActivity.java
+++ app/src/main/java/com/olezhko/lpmldemo/MainActivity.java
@@ -1,14 +1,15 @@
 package com.olezhko.lpmldemo;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.app.AlertDialog;
 import android.app.Dialog;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.EditText;
 import android.widget.ListView;
@@ -20,14 +21,17 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+    FloatingActionButton fab;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        //catching incoming intent
         Intent incomingIntent = getIntent();
+        //getting our user data fro it
         String[] userData = incomingIntent.getStringArrayExtra("userData");
+        //displaying email in the Toast
         Toast.makeText(this, "email:" + userData[0], Toast.LENGTH_LONG).show();
 
         List<Cafe> cafes = new ArrayList<>();
@@ -40,25 +44,41 @@ public class MainActivity extends AppCompatActivity {
         ArrayAdapter cafeAdapter = new CafeAdapter(this, cafes);
         cafeListView.setAdapter(cafeAdapter);
 
-        FloatingActionButton fab = findViewById(R.id.fab);
+        fab = findViewById(R.id.fab);
+        //initializing View for dialog along with editTexts
         LayoutInflater inflater = getLayoutInflater();
-        View view = inflater.inflate(R.layout.dialog_inside, null);
-        fab.setOnClickListener(v -> {
-            AlertDialog.Builder builder = new AlertDialog.Builder(this);
-            builder.setNegativeButton("Cancel", (dialog, which) -> dialog.cancel())
-                    .setPositiveButton("AddCafe", ((dialog, which) -> {
-                        EditText nameEditText = view.findViewById(R.id.cafe_name_text);
-                        EditText addressEditText = view.findViewById(R.id.cafe_address_text);
-                        String name = nameEditText.getText().toString();
-                        String address = addressEditText.getText().toString();
-                        cafes.add(new Cafe(name, address, R.drawable.cafe));
-                        dialog.dismiss();
-                    })).setView(view)
-                    .setTitle("Choose Your Poison")
-                    .create()
-                    .show();
-        });
+        View dialogView = inflater.inflate(R.layout.dialog_contents, null);
+        EditText cafeNameEditText = dialogView.findViewById(R.id.name);
+        EditText cafeAddressEditText = dialogView.findViewById(R.id.address);
 
+        AlertDialog.Builder builder = new AlertDialog.Builder(this).setTitle("Choose your poison")
+                .setView(dialogView)
+                .setNegativeButton("Cancel", ((dialog, which) -> dialog.cancel()))
+                .setPositiveButton("Add Cafe", ((dialog, which) -> {
+                    //getting data from editTexts
+                    String name = cafeNameEditText.getText().toString();
+                    String address = cafeAddressEditText.getText().toString();
+                    cafes.add(new Cafe(name, address, R.drawable.cafe));
+                    /* Notifying adapter that data changes and making
+                        it do necessary changes inside
+                    */
+                    cafeAdapter.notifyDataSetChanged();
+                    dialog.dismiss();
+
+                }));
+
+        fab.setOnClickListener(v -> {
+            /*
+                As view is set into dialog it gains parent so we removing it if it exists
+             */
+            if (dialogView.getParent() != null) {
+                ((ViewGroup) dialogView.getParent()).removeView(dialogView);
+                //clearing editTexts from previous data
+                cafeNameEditText.setText("");
+                cafeAddressEditText.setText("");
+            }
+            builder.create().show();
+        });
     }
 }
 
